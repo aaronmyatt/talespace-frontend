7512f7563dc8cc3741fbd3082128aecba9f1d716
Aaron Myatt <aaronmyatt@gmail.com>
2016-12-08 22:28:20 +0800

    install ngbootstrap use boostrap cdn simple styles for about page add new packages to package.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {RouterModule} from "@angular/router";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {AppComponent} from "./components/app/app.component";
+import {HomeComponent} from "./components/home/home.component";
+import {AboutComponent} from "./components/about/about.component";
+import {ContactComponent} from "./components/contact/contact.component";
+import {DonationComponent} from "./components/donation/donation.component";
+import {AuthComponent} from "./components/auth/auth.component";
+import {ProfileComponent} from "./components/profile/profile.component";
+import {ListComponent} from "./components/list/list.component";
+import {GalleryComponent} from "./components/gallery/gallery.component";
+import {NavComponent} from "./components/navigation/nav.component";
+import {appRoutes} from "./routing/routes";
 
 
 // Components
-import { AppComponent } from './components/app/app.component';
-import { HomeComponent } from './components/home/home.component';
-import { AboutComponent } from './components/about/about.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { DonationComponent } from './components/donation/donation.component';
-import { AuthComponent } from './components/auth/auth.component';
-import { ProfileComponent } from './components/profile/profile.component';
-import { ListComponent } from './components/list/list.component';
-import { GalleryComponent } from './components/gallery/gallery.component';
-import { NavComponent } from './components/navigation/nav.component';
 
 // Routing
-import { appRoutes } from './routing/routes';
 
 @NgModule({
   declarations: [
@@ -37,7 +38,8 @@ import { appRoutes } from './routing/routes';
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
